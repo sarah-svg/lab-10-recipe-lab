f57c921daee33e0c25ec80bc838477b359ffda08
sarah-svg <kjskillen@gmail.com>
2020-12-15 13:15:55 -0800

    delete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -49,13 +49,6 @@ describe('recipe-lab routes', () => {
     expect(response.body).toEqual(recipe);
   });
 
-
-
-
-
-
-
-
   it('gets all recipes', async() => {
     const recipes = await Promise.all([
       { name: 'cookies', directions: [] },
@@ -107,4 +100,29 @@ describe('recipe-lab routes', () => {
         });
       });
   });
+  it('DELETES tea by id', async() => {
+    const recipe = await Recipe.insert({
+      name: 'good cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+    
+    const response = await request(app)
+      
+      .delete(`/api/v1/recipes/${recipe.id}`);
+
+    expect(response.body).toEqual(recipe);
+  });
+
+
+
+
+
+
+
+
 });
