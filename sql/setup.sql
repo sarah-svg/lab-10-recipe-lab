8c92bcf10204ab2b83ea58fb7d67d9f5960b6641
sarah-svg <kjskillen@gmail.com>
2020-12-15 15:19:35 -0800

    log model with crud routes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,17 @@
 DROP TABLE IF EXISTS recipes;
-
+DROP TABLE IF EXISTS logs;
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL,
   directions TEXT[]
 );
+  
+
+
+CREATE TABLE logs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  recipe_id int,
+  date_of_event DATE,
+  notes TEXT NOT NULL,
+  rating INTEGER NOT NULL
+)
