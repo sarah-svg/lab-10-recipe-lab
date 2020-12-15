e1dac9ccdb0aacd59d7256a06706c21b36806f13
sarah-svg <kjskillen@gmail.com>
2020-12-15 13:12:41 -0800

    get recipes by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,11 @@ describe('recipe-lab routes', () => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
+  afterAll(() => {
+    return pool.end();
+
+  });
+
   it('creates a recipe', () => {
     return request(app)
       .post('/api/v1/recipes')
@@ -34,6 +39,22 @@ describe('recipe-lab routes', () => {
         });
       });
   });
+
+  it('finds a recipes by id via GET', async() => {
+    const recipe = await Recipe.insert({ name: 'cookies', directions: [] });
+
+    const response = await request(app)
+      .get(`/api/v1/recipes/${recipe.id}`);
+
+    expect(response.body).toEqual(recipe);
+  });
+
+
+
+
+
+
+
 
   it('gets all recipes', async() => {
     const recipes = await Promise.all([
