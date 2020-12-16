cc419fe297cc7b4a544a599b93350df16a275643
sarah-svg <kjskillen@gmail.com>
2020-12-15 17:18:04 -0800

    test passing with join

--- __tests__/recipes.test.js
+++ __tests__/recipes.test.js
@@ -3,10 +3,27 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const Recipe = require('../lib/models/recipe');
+const Log = require('../lib/models/logs');
 
 describe('recipe-lab routes', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  // beforeEach(() => {
+  //   return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  // });
+ 
+  let recipe;
+  beforeEach(async() => {
+  
+    await pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+    recipe = await Recipe.insert({
+       
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
   });
 
   afterAll(() => {
@@ -40,14 +57,77 @@ describe('recipe-lab routes', () => {
       });
   });
 
-  it('finds a recipes by id via GET', async() => {
-    const recipe = await Recipe.insert({ name: 'cookies', directions: [] });
+  // it('finds a recipes by id via GET', async() => {
+  //   const recipe = await Recipe.insert({ name: 'cookies', directions: [] });
 
+  //   const response = await request(app)
+  //     .get(`/api/v1/recipes/${recipe.id}`);
+
+  //   expect(response.body).toEqual(recipe);
+  // });
+
+
+  it('finds a recipes by id via GET', async() => {
+
+    await Promise.all([
+      {
+        dateOfEvent: '2020-12-10',
+        notes: 'hey',
+        rating: 2,
+        recipeId: recipe.id,
+ 
+      },
+      {
+        dateOfEvent: '2020-12-10',
+        notes: 'hey',
+        rating: 2,
+        recipeId: recipe.id,
+ 
+      },
+      {
+        dateOfEvent: '2020-12-10',
+        notes: 'hey',
+        rating: 2,
+        recipeId: recipe.id,
+ 
+      },
+    ].map(log => Log.insert(log))
+    );
     const response = await request(app)
       .get(`/api/v1/recipes/${recipe.id}`);
 
-    expect(response.body).toEqual(recipe);
+    // const recipe = await Recipe.insert({ name: 'cookies', directions: [] });
+
+
+
+    expect(response.body).toEqual({ ...recipe, logs: expect.arrayContaining([
+      {
+        id: '1',
+        dateOfEvent: '2020-12-10',
+        notes: 'hey',
+        rating: 2,
+        recipeId: recipe.id,
+ 
+      },
+      {
+        id: '2',
+        dateOfEvent: '2020-12-10',
+        notes: 'hey',
+        rating: 2,
+        recipeId: recipe.id,
+ 
+      },
+      {
+        id: '3',
+        dateOfEvent: '2020-12-10',
+        notes: 'hey',
+        rating: 2,
+        recipeId: recipe.id,
+ 
+      }
+    ]) });
   });
+
 
   it('gets all recipes', async() => {
     const recipes = await Promise.all([
@@ -100,7 +180,7 @@ describe('recipe-lab routes', () => {
         });
       });
   });
-  it('DELETES tea by id', async() => {
+  it('DELETES recipe by id', async() => {
     const recipe = await Recipe.insert({
       name: 'good cookies',
       directions: [
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,21 +1,29 @@
-DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS recipes CASCADE;
 DROP TABLE IF EXISTS logs;
 
 CREATE TABLE recipes (
-  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name TEXT NOT NULL,
   directions TEXT[]
 );
   
 
 
+-- CREATE TABLE logs (
+--   id BIGINT GENERATED ALWAYS AS IDENTITY,
+--   -- recipe_id BIGINT NOT NULL REFERENCES recipe(id),
+--   recipe_id int,
+--   date_of_event DATE,
+--   notes TEXT NOT NULL,
+--   rating INTEGER NOT NULL
+-- )
 CREATE TABLE logs (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
-  recipe_id int,
-  date_of_event DATE,
-  notes TEXT NOT NULL,
-  rating INTEGER NOT NULL
-)
+  date_of_event  DATE NOT NULL,
+  notes TEXT NOT NULL, 
+  rating INT CONSTRAINT rating CHECK (rating >= 0 AND rating <= 5),
+  recipe_id BIGINT NOT NULL REFERENCES recipes(id)
+);
 -- Our model is incomplete.
 --  In order to provide a better user 
 --  experience our recipes should include 
